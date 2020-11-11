7dcb8b78f4d0d1f0510ee7d929dee9dd22ecc3c4
LionelBeato <lionel.beato@gmail.com>
2020-11-11 18:20:36 -0500

    add json dependency

--- src/main/java/com/collab/todoAppDemo/Services/TodoListService.java
+++ src/main/java/com/collab/todoAppDemo/Services/TodoListService.java
@@ -1,4 +1,28 @@
 package com.collab.todoAppDemo.Services;
 
+import com.collab.todoAppDemo.Models.TodoList;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Optional;
+
 public interface TodoListService {
+
+    // get all
+
+    Iterable<TodoList> getAllToDoLists();
+
+    // get one by id
+
+    Optional<TodoList> getToDoListById();
+
+    // delete all
+
+    JsonFormat deleteAllToDoLists();
+
+    // delete by id
+
+    // update by id
+
+    // post todolist
+
 }
