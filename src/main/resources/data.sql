dcc92910dc957719bd56fb5cefececa5a4c96ace
SayeedJ <sayeedjoseph@gmail.com>
2020-11-11 19:43:26 -0500

    created sql files with data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tasks(id, description, is_complete, todo_lists_id)
+VALUES ("12345678-1234-5678-1234-567812345777", "Clean Room", false, "12345678-1234-5678-1234-567812345678")
+
+INSERT INTO todo_lists(id, title)
+VALUES ("12345678-1234-5678-1234-567812345678", "General Tasks")
