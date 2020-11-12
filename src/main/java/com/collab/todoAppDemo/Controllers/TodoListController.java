ced11ce18b7038a7ee9ce419ffe6eee8b9e3b8ca
LionelBeato <lionel.beato@gmail.com>
2020-11-11 20:32:36 -0500

    implement todolistController

--- src/main/java/com/collab/todoAppDemo/Controllers/TodoListController.java
+++ src/main/java/com/collab/todoAppDemo/Controllers/TodoListController.java
@@ -1,17 +1,50 @@
 package com.collab.todoAppDemo.Controllers;
 
+import com.collab.todoAppDemo.Models.TodoList;
+import com.collab.todoAppDemo.Services.TodoListService;
+import org.json.simple.JSONObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/todolist")
 public class TodoListController {
 
-    // get
+        TodoListService TodoListService;
 
-    // get all
+    public TodoListController(TodoListService TodoListService){
+        this.TodoListService = TodoListService;
+    }
 
-    // post
+    @GetMapping("/get/{id}")
+    public Optional<TodoList> getTodoListById(@PathVariable UUID id){
+        return TodoListService.getToDoListById(id);
+    }
 
-    // put
+    @GetMapping("/get/all")
+    public Iterable<TodoList> getAllTodoLists(){
+        return TodoListService.getAllToDoLists();
+    }
 
-    // delete
+    @PostMapping("/post")
+    public JSONObject postTodoList(@RequestBody TodoList TodoList){
+        return  TodoListService.postToDoList(TodoList);
+    }
 
-    // delete all
+    @PutMapping("/put/{id}")
+    public JSONObject putTodoList(@PathVariable UUID id,  @RequestBody TodoList TodoList){
+        return TodoListService.updateToDoListById(id, TodoList);
+    }
 
+    @DeleteMapping("/delete/{id}")
+    public JSONObject deleteTodoList(@PathVariable UUID id){
+        return TodoListService.deleteToDoListById(id);
+    }
+
+    @DeleteMapping("/delete/all")
+    public JSONObject deleteAllTodoLists(){
+        return TodoListService.deleteAllToDoLists();
+    }
 }
